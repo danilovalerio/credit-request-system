049b7254f6cf2ca24c797b8aa7403078506a7684
danilovalerio <danilo_vs@hotmail.com>
2023-03-28 14:14:40 -0300

    Anotacoes para o hibernate

--- src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/entity/Address.kt
+++ src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/entity/Address.kt
@@ -1,6 +1,12 @@
 package com.projetos.danilo.systemcredit.creditrequestsystem.entity
 
+import jakarta.persistence.Embeddable
+
+/**
+ * Não vou criar uma tabela de endereço
+ * Vou pegar a classe de address integrar com customer
+ */
+@Embeddable
 data class Address (
     var zipCode: String = ""
-
 )
--- src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/entity/Customer.kt
+++ src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/entity/Customer.kt
@@ -1,12 +1,32 @@
 package com.projetos.danilo.systemcredit.creditrequestsystem.entity
 
+import jakarta.persistence.*
+
+/**
+ * Essa classe será uma tabela no banco de dados
+ * Para nomear diferente @Table(name = "Cliente")
+ */
+@Entity
 data class Customer(
+    @Column(nullable = false)
     var firstName: String = "",
+    @Column(nullable = false)
     var lastName: String = "",
+    @Column(nullable = false, unique = true)
     var cpf: String = "",
+    @Column(nullable = false, unique = true)
     var email: String = "",
+    @Column(nullable = false)
     var password: String = "",
+    // Embedded - Integrado address dentro de customer
+    @Column(nullable = false) @Embedded
     var address: Address = Address(),
+    // FetchType Lazy: Carregar lista de creditos só quando quiser, não quando carregar
+    // Cascade: Para limpar a lista de creditos caso apague esse ususario
+    @Column(nullable = false)
+    @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
+    mappedBy = "customer")
     var credits: List<Credit> = mutableListOf(),
-    var id: Long? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
 )
