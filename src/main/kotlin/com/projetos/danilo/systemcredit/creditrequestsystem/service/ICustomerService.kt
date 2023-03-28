7967f7c158cfb5b013c7b64294775daf0c37dfab
danilovalerio <danilo_vs@hotmail.com>
2023-03-28 14:57:12 -0300

    Service Customer - funcionalidades

---
+++ src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/service/ICustomerService.kt
@@ -0,0 +1,10 @@
+package com.projetos.danilo.systemcredit.creditrequestsystem.service
+
+import com.projetos.danilo.systemcredit.creditrequestsystem.entity.Customer
+
+interface ICustomerService {
+    fun save(customer: Customer): Customer
+    fun findById(id: Long): Customer
+    fun delete(id: Long): Customer
+
+}
