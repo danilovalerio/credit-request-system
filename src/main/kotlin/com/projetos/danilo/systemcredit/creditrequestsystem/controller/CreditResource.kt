767348090e433afc4b8916a51fe9f686b3af369d
danilovalerio <danilo_vs@hotmail.com>
2023-03-28 17:01:32 -0300

    fix - credit code pathvariable

--- src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/controller/CreditResource.kt
+++ src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/controller/CreditResource.kt
@@ -34,7 +34,7 @@ class CreditResource (
             .collect(Collectors.toList())
     }
 
-    @GetMapping
+    @GetMapping("/{creditCode}")
     fun findByCreditCode(@RequestParam(value = "customerId") customerId: Long,
                          @PathVariable creditCode: UUID) : CreditView {
         val credit: Credit = this.creditService.findByCreditCode(customerId, creditCode)
