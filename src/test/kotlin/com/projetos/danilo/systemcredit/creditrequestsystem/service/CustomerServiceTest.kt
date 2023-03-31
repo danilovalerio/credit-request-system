36cc731a1c3288752282a73e9ce05050ee22861a
danilovalerio <danilo_vs@hotmail.com>
2023-03-31 17:59:41 -0300

    Config de ActiveProfiles para test e ExtendWith para usar o MockK

---
+++ src/test/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/service/CustomerServiceTest.kt
@@ -0,0 +1,11 @@
+package com.projetos.danilo.systemcredit.creditrequestsystem.service
+
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@ExtendWith(MockKExtension::class)
+class CustomerServiceTest {
+
+}
