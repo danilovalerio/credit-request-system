785d58437ddc31afcffd8b9213208d40e4220619
danilovalerio <danilo_vs@hotmail.com>
2023-03-31 16:48:06 -0300

    indicando url de documentacao mais a classe de configuracao do swagger

---
+++ src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/configuration/Swagger3Config.kt
@@ -0,0 +1,7 @@
+package com.projetos.danilo.systemcredit.creditrequestsystem.configuration
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Swagger3Config {
+}
