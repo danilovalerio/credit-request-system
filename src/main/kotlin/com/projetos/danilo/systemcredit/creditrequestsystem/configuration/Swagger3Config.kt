597518fb737427fdcdd52b23f2a6d78a4f5b7ce3
danilovalerio <danilo_vs@hotmail.com>
2023-03-31 17:06:12 -0300

    documentacao com swagger

--- src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/configuration/Swagger3Config.kt
+++ src/main/kotlin/com/projetos/danilo/systemcredit/creditrequestsystem/configuration/Swagger3Config.kt
@@ -1,7 +1,16 @@
 package com.projetos.danilo.systemcredit.creditrequestsystem.configuration
 
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class Swagger3Config {
+    @Bean
+    fun publicApi(): GroupedOpenApi? {
+        return GroupedOpenApi.builder()
+            .group("springcreditapplicationsystem-public")
+            .pathsToMatch("/api/customers/**", "/api/credits/**")
+            .build()
+    }
 }
